c1878662015df40d9fdc75a2e37b90b297c2344d
fast1sh <34493866+fast1sh@users.noreply.github.com>
2019-01-22 19:49:46 +0300

    Add files via upload

---
+++ todo-app/build/precache-manifest.d2a277b40fe6dc2051f373d56e0eb75d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7c4dcd0184bf51c8512d",
+    "url": "/static/js/main.7c4dcd01.chunk.js"
+  },
+  {
+    "revision": "6356b65c008adadcb202",
+    "url": "/static/js/1.6356b65c.chunk.js"
+  },
+  {
+    "revision": "7c4dcd0184bf51c8512d",
+    "url": "/static/css/main.7763fdc5.chunk.css"
+  },
+  {
+    "revision": "1a5d0f99821354f0af078d0ddcdd9008",
+    "url": "/index.html"
+  }
+];
